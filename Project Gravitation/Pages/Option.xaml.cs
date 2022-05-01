03305e97aa7b7ab7f2b7e0f02a739f9ebf5579ca
gnlckswjd <gnlckswjd1@gmail.com>
2022-05-01 12:00:35 +0900

    day1
    1, 1.2 틀 생성

---
+++ Project Gravitation/Pages/Option.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project_Gravitation.Pages
+{
+    /// <summary>
+    /// Option.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Option : Page
+    {
+        public Page sourcepage;
+        public Option()
+        {
+            InitializeComponent();
+          
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(sourcepage);
+
+        }
+    }
+}
