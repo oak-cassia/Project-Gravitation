f0095508b24fb42b949117adfd7df11b0a1a109f
gnlckswjd <gnlckswjd1@gmail.com>
2022-05-15 12:40:06 +0900

    day4_2
    변수명 직관적으로 수정 및 새로 만든 버튼에 커맨드 생성

--- ProjectGravitation/ProjectGravitation/ProjectGravitation/Commands/GameCommand.cs
+++ ProjectGravitation/ProjectGravitation/ProjectGravitation/Commands/GameCommand.cs
@@ -33,16 +33,23 @@ namespace ProjectGravitation.Commands
             MyButton clickedBtn = parameter as MyButton; // MyButton 으로 기본 스타일을 정해두었습니다. 버튼기본값 바꾸는 게 어려워서;;
             if (clickedBtn.Content.ToString() == "게임 시작")
                 MainStart(clickedBtn);   // 각 버튼마다 다른 함수를 호출하게 합니다. 
+            if (clickedBtn.Content.ToString() == "1지역")
+                SectorOneStart(clickedBtn);
         }
 
-        public void MainStart(Button buttons)
+        public void MainStart(Button clickedBtn)
         {
-            StackPanel aa = buttons.Parent as StackPanel;//버튼의 부모인 스택패널 buttons에 접근
-            aa.Children.Clear();                    //  자식을 지우고  버튼을 추가해주면 됩니다.
+            StackPanel panel = clickedBtn.Parent as StackPanel;//버튼의 부모인 스택패널 buttons에 접근
+            panel.Children.Clear();                    //  자식을 지우고  버튼을 추가해주면 됩니다.
             MyButton button1 = new MyButton();
             button1.Content = "1지역";
-
-            aa.Children.Add(button1);
+            button1.Command=_game._gameCommand;
+            button1.CommandParameter = button1;
+            panel.Children.Add(button1);
+        }
+        public void SectorOneStart(Button clickedBtn)
+        {
+            _game.Text = "이게되나";
         }
 
         
